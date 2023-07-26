d53ab6262a16b5dab4c26826736d894f6ea6bfea
Reuben Hawley <95055130+xTr161@users.noreply.github.com>
2023-07-26 13:22:33 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,13 @@
+// Create web server 
+{
+    const http = require('http');
+    const fs = require('fs');
+    const url = require('url');
+    const qs = require('querystring');
+    const template = require('./lib/template.js');
+    const path = require('path');
+    const sanitizeHtml = require('sanitize-html');
+
+    const app = http.createServer(function (request, response) {
+});
+}
